bbb6b1c5be289409a9d099e3249617cdfccb3f4b
shashiweb <shashiweb64@gmail.com>
2024-10-10 12:29:14 +0530

    login

--- client/src/modules/auth/Myloginpage.js
+++ client/src/modules/auth/Myloginpage.js
@@ -1,7 +1,63 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Myloginpage() {
+    const navigat =useNavigate();
+    const [login,setlogin]=useState({
+        email:"",
+        pass:""
+    });
+
+    const loginuseer = (e)=>{
+        console.log(e.target.value);
+        const {name,value} = e.target;
+        setlogin((a)=>{
+          return{
+            ...a,
+            [name]:value
+          }
+        })
+    }
+
+    const userlogin = async()=>{
+        const {email,pass}=login;
+        if(login.email==="" || login.pass==="")
+        {
+            alert("Email id and Password is blank!");
+        }
+        else
+        {
+            const datares = await fetch("http://localhost:8900/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    email, pass
+                })
+            });
+            const resdata = await datares.json();
+            console.log(resdata);
+            if(resdata.status===220)
+            {
+                alert("welcome to");
+                navigat('/dashboard');
+            }
+
+            if(resdata.status===620)
+            {
+                alert("user not found");
+            }
+
+            if(resdata.status===421)
+                {
+                    alert("email and password don't match");
+                }
+        }
+
+    }
+
+
+
+
   return (
     <div className='container'>
     <div className='row justify-content-center'>
@@ -9,27 +65,27 @@ function Myloginpage() {
             <div className='container-fluid'>
                 <div className='row'>
                 <div className='col-12 text-center'>
-                        <div class="mb-3">
+                        <div className="mb-3">
                           <p className='h2'>Welcome to Login page</p>
                         </div>
                     </div>
                     <div className='col-12'>
-                        <div class="mb-3">
-                            <label class="form-label">Email address</label>
-                            <input type="email" class="form-control" />
+                        <div className="mb-3">
+                            <label className="form-label">Email address</label>
+                            <input type="email" className="form-control" name='email' value={login.email} onInput={loginuseer}/>
                         </div>
                     </div>
                     <div className='col-12'>
-                        <div class="mb-3">
-                            <label class="form-label">Password</label>
-                            <input type="password" class="form-control" />
+                        <div className="mb-3">
+                            <label className="form-label">Password</label>
+                            <input type="password" className="form-control" name='pass' value={login.pass} onInput={loginuseer}/>
                         </div>
                     </div>
                     <div className='col-12 text-center'>
-                        <div class="mb-3">
-                           <button className='btn btn-success c-btn'>login</button>
+                        <div className="mb-3">
+                           <button className='btn btn-success c-btn' type="button" onClick={userlogin}>login</button>
                            <Link to="registor" className="btn">Registor Now</Link>
-                           <Link to="dashboard" className="btn">dashboard</Link>
+                           {/* <Link to="dashboard" className="btn">dashboard</Link> */}
                         </div>
                     </div>
                 </div>
--- server/routes/myroute.js
+++ server/routes/myroute.js
@@ -3,25 +3,25 @@ const express = require('express');
 const apps = express.Router();
 const myschimatype = require('../myschimatype/common');
 
-apps.get("/",(req,res)=>{
+apps.get("/", (req, res) => {
     res.send("welcome to express js");
 });
 
-apps.get("/about",(req,res)=>{
+apps.get("/about", (req, res) => {
     res.send("welcome about js");
 });
 
 
-apps.get("/alldata",async(req,res)=>{
-   const  alld = await myschimatype.find();
-   res.send(alld);
+apps.get("/alldata", async (req, res) => {
+    const alld = await myschimatype.find();
+    res.send(alld);
 });
 
 
-apps.post("/registoruser",async(req,res)=>{
-    const {email,fullname,phone,course,dob,pass} = req.body;
+apps.post("/registoruser", async (req, res) => {
+    const { email, fullname, phone, course, dob, pass } = req.body;
     const adduser = new myschimatype({
-        email,fullname,phone,course,dob,pass
+        email, fullname, phone, course, dob, pass
     });
     await adduser.save();
     res.status(200).json(adduser);
@@ -29,32 +29,58 @@ apps.post("/registoruser",async(req,res)=>{
 });
 
 
-apps.delete("/deleterecord/:id",async(req,res)=>{
-    const {id} = req.params;
-        const del = await myschimatype.findByIdAndDelete({"_id":id});
-        console.log(del);
-        res.status(256).json(del);
+apps.delete("/deleterecord/:id", async (req, res) => {
+    const { id } = req.params;
+    const del = await myschimatype.findByIdAndDelete({ "_id": id });
+    console.log(del);
+    res.status(256).json(del);
 });
 
-apps.get("/singleuser/:id", async(req,res)=>{
-    const {id} = req.params;
-    const users = await myschimatype.findOne({_id:id});
-        res.status(288).json(users);
+apps.get("/singleuser/:id", async (req, res) => {
+    const { id } = req.params;
+    const users = await myschimatype.findOne({ _id: id });
+    res.status(288).json(users);
 });
 
 
-apps.patch("/updateuser/:id", async(req,res)=>{
-    const {id} = req.params;
-    const recordupdate = await myschimatype.findByIdAndUpdate(id,req.body,{new:true});
+apps.patch("/updateuser/:id", async (req, res) => {
+    const { id } = req.params;
+    const recordupdate = await myschimatype.findByIdAndUpdate(id, req.body, { new: true });
     console.log(recordupdate);
     res.status(201).json(recordupdate);
 });
 
 
 
+apps.post("/login", async (req, res) => {
+    const { email, pass } = req.body;
+    if (email === "" || pass === "") {
+        res.status(412).json({ message: "error email and pass", status: 420 });
+    }
+    else {
+        const logindetails = await myschimatype.findOne({ email: email });
+        console.log(logindetails);
+        if (logindetails) {
+            if (logindetails.email === email && logindetails.pass === pass) {
+                res.status(200).json({ message: "welcome", status: 220 });
+            }
+            else {
+                res.status(300).json({ message: "email and password don't match", status: 421 });
+            }
+        }
+        else {
+            res.status(300).json({ message: "error", status: 620 });
+        }
+
+    }
+});
 
 
-module.exports= apps;
+
+
+
+
+module.exports = apps;
 
 
 
