9a4ab3043d24456828d8093327efc5f3f3667b4e
shashiweb <shashiweb64@gmail.com>
2024-10-16 12:31:37 +0530

    change

--- client/src/modules/shares/Myapi.js
+++ client/src/modules/shares/Myapi.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-//   const Myapi = "https://codemern11.onrender.com";
-  const Myapi="http://localhost:8900";
+  const Myapi = "https://codemern11.onrender.com";
+  // const Myapi="http://localhost:8900";
   export default Myapi;
